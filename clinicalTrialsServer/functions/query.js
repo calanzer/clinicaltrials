3a1f11198735acf242bd29b69f0daf745fa94d5b
calanzer <calanzer@gmail.com>
2019-11-13 17:49:03 -0800

    Query

--- clinicalTrialsServer/functions/query.js
+++ clinicalTrialsServer/functions/query.js
@@ -1,8 +1,23 @@
-const functions = require('firebase-functions');
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+
+
+exports.helloWorld = (req, res) => {
+    const admin = require('firebase-admin');
+    const functions = require('firebase-functions');
+  
+    admin.initializeApp(functions.config().firebase);
+    let db = admin.firestore()
+    
+    let message = req.query.message || req.body.message || 'Hello World!';
+    var database = db.collection("All Studies")
+    var query = database.where("overall_status", "==", "Completed");
+    
+    
+    res.status(200).send(query);
+  };
