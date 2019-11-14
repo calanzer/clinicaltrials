ccf5e27615f488e96a6d73cd04b19c4c19bd8c4f
calanzer <calanzer@gmail.com>
2019-11-13 19:11:49 -0800

    query

--- clinicalTrialsServer/functions/index.js
+++ clinicalTrialsServer/functions/index.js
@@ -15,7 +15,7 @@ const functions = require('firebase-functions');
   
     admin.initializeApp(functions.config().firebase);
     let db = admin.firestore()
-    
+    let result
     var database = db.collection("All Studies")
     var query = database.where("overall_status", "==", "Completed").get()
         .then(snapshot => {
@@ -24,12 +24,16 @@ const functions = require('firebase-functions');
                 return;
             }
             snapshot.forEach(doc => {
-                console.log(doc.id, '=>', doc.data());
+                result = doc.id
+                details = doc.data().brief_title
+
             })
+            console.log(details)
+            res.status(200).send(details);
         })
         .catch(err => {
             console.log('Error getting documents', err);
         });
     
-    res.status(200).send(query);
+    
   });
