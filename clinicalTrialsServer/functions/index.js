affcbee1b1a884ec9cf3ed33012382a418f23c93
calanzer <calanzer@gmail.com>
2019-11-13 18:06:38 -0800

    query

--- clinicalTrialsServer/functions/index.js
+++ clinicalTrialsServer/functions/index.js
@@ -4,5 +4,21 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
  exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+    sympton = request.parameter(symptom)
+
+    response.send("Hello from Firebase!");
  });
+
+ exports.query = functions.https.onRequest((req, res) => {
+    const admin = require('firebase-admin');
+    const functions = require('firebase-functions');
+  
+    admin.initializeApp(functions.config().firebase);
+    let db = admin.firestore()
+    
+    var database = db.collection("All Studies")
+    var query = database.where("overall_status", "==", "Completed");
+    
+    
+    res.status(200).send(query);
+  });
