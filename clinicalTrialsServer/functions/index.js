e71f9d0f29667855949e326035364ce37e73e2d7
calanzer <calanzer@gmail.com>
2019-11-17 17:05:36 -0800

    cors

--- clinicalTrialsServer/functions/index.js
+++ clinicalTrialsServer/functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-
+const cors = require('cors')({ origin: true})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -10,6 +10,7 @@ const functions = require('firebase-functions');
  });
 
  exports.query = functions.https.onRequest((req, res) => {
+    return cors(req, res, () => {
     const admin = require('firebase-admin');
     const functions = require('firebase-functions');
     const arrayOfResults = new Array()
@@ -29,11 +30,14 @@ const functions = require('firebase-functions');
               
             })
             //console.log(arrayOfResults)
+            res.set('Access-Control-Allow-Origin', "*")
+            res.set('Access-Control-Allow-Methods', 'GET, POST')
             res.status(200).send(arrayOfResults);
+            return
         })
         .catch(err => {
             console.log('Error getting documents', err);
         });
-    
+    })
     
   });
