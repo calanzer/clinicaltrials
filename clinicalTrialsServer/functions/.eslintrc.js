164e435f38232eddf58ffa586ec2da358e6da227
Christian Lanzer <christianlanzer@Christians-MBP.lan>
2021-02-02 01:26:40 -0700

    deployed api

--- clinicalTrialsServer/functions/.eslintrc.js
+++ clinicalTrialsServer/functions/.eslintrc.js
@@ -4,8 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "google",
+    // "eslint:recommended"
   ],
   rules: {
     quotes: ["error", "double"],
--- clinicalTrialsServer/functions/index.js
+++ clinicalTrialsServer/functions/index.js
@@ -1,18 +1,19 @@
 const functions = require("firebase-functions");
-const cors = require('cors')({ origin: '*'})
+const cors = require("cors")({origin: "*"});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  let sympton = request.parameter(symptom)
+  const sympton = request.parameter(symptom)
   response.send("Hello from Firebase!");
 });
+
 exports.query = functions.https.onRequest((req, res) => {
   return cors(req, res, () => {
-  const admin = require('firebase-admin');
-  const functions = require('firebase-functions');
-  const arrayOfResults = new Array()
+  const admin = require("firebase-admin");
+  const functions = require("firebase-functions");
+  const arrayOfResults = new Array();
   //const firebaseID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
   if (!admin.apps.length) {admin.initializeApp(functions.config().firebase)}
   let db = admin.firestore()
@@ -26,11 +27,11 @@ exports.query = functions.https.onRequest((req, res) => {
   var query = database.where("overall_status", "==", querySearchTermOne).get()
       .then(snapshot => {
           if (snapshot.empty) {
-              console.log('No matching documents');
+              console.log("No matching documents");
               arrayOfResults.push("No Results Found")
-              res.set('Access-Control-Allow-Origin', "*")
-              res.set('Access-Control-Allow-Methods', "GET, POST, PATCH, PUT, DELETE, OPTIONS")
-              res.set('Access-Control-Allow-Headers', "Origin, Content-Type")
+              res.set("Access-Control-Allow-Origin", "*")
+              res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+              res.set("Access-Control-Allow-Headers", "Origin, Content-Type")
               res.status(404).send(arrayOfResults);
           return
           }
@@ -38,14 +39,14 @@ exports.query = functions.https.onRequest((req, res) => {
               arrayOfResults.push(doc.data())
           })
           //console.log(arrayOfResults)
-          res.set('Access-Control-Allow-Origin', "*")
-          res.set('Access-Control-Allow-Methods', "GET, POST, PATCH, PUT, DELETE, OPTIONS")
-          res.set('Access-Control-Allow-Headers', "Origin, Content-Type")
+          res.set("Access-Control-Allow-Origin", "*")
+          res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+          res.set("Access-Control-Allow-Headers", "Origin, Content-Type")
           res.status(200).send(arrayOfResults);
           return
       })
       .catch(err => {
-          console.log('Error getting documents', err);
+          console.log("Error getting documents", err);
       });
   })
   
